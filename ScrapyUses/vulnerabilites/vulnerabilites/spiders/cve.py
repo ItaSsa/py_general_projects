bc7163e22c57e5752697dc4bb79afd8b99b24c03
ItaSsa <itairassa@gmail.com>
2025-01-21 19:46:14 +0100

    scraping local files

--- ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
+++ ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
@@ -1,5 +1,5 @@
-import scrapy
 '''
+    Version 1 - scriping from a website - Notes:
     After:
         - Create project: scrapy startproject vulnerabilities
         - Go to new project directory
@@ -9,12 +9,23 @@ import scrapy
         cd /home/itaira/proj_py/py_general_projects/ScrapyUses/vulnerabilities
     2. run scrapy crawl cve
     
+    Version 2 - scraping local files - Notes:
+        1. In the terminal make the download:  wget https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html
+        2. Use OS to get file
+        Try to time and compare scraping times from website and localy: time scrapy crawl cve
 '''
+import scrapy
+import os
+
+# Directory of the actual file
+current_dir = os.path.dirname(__file__)
+url = os.path.join(current_dir,'source-EXPLOIT-DB.html')
 
 class CveSpider(scrapy.Spider):
     name = "cve"
     allowed_domains = ["cve.mitre.org"]
-    start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+    # start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+    start_urls = [f"file://{url}"]
 
     def parse(self, response):
         #Identifying the interesting table
