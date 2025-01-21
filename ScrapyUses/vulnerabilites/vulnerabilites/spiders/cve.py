5c17ec06a31da8f2403fabaeb2e52514d0c655e2
ItaSsa <itairassa@gmail.com>
2025-01-21 22:20:04 +0100

    Persisting Sqlite

--- ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
+++ ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
@@ -14,19 +14,20 @@
         2. Use OS to get file
         Try to time and compare scraping times from website and localy: time scrapy crawl cve
 
-    Version 3 - Saving scraping data as a csv file - Notes:
-        1. import csv module
-        2. Creating a csv file 
-        3. Using writerow to write rows
+    Version 3 - Saving scraping data as a csv file
     
-    Version 4 - Saving scriping data as a json file - Notes:
-        1. import json
+    Version 4 - Saving scriping data as a json file 
+
+    Vesion 5 - Persisting Sqlite 
+
+
 '''
 import scrapy
 import os
 import csv
 import re
 import json
+import sqlite3
 
 # Directory of the actual file
 current_dir = os.path.dirname(__file__)
@@ -35,48 +36,60 @@ url = os.path.join(current_dir,'source-EXPLOIT-DB.html')
 class CveSpider(scrapy.Spider):
     name = "cve"
     allowed_domains = ["cve.mitre.org"]
-    # start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
-    start_urls = [f"file://{url}"]
+    start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+    # start_urls = [f"file://{url}"]
 
+            
     def parse(self, response):
+
+        
+        ####Version 5
+        ##### sqlite config
+        connection = sqlite3.connect('vulnb.db')
+        table = 'CREATE TABLE vulns (exploit TEXT, cve TEXT)'
+        cursor = connection.cursor()
+        cursor.execute(table)
+        connection.commit()
+       
+
         #Identifying the interesting table
         for child in response.xpath('//table'):
             if len(child.xpath('tr'))>100:
                 table = child
                 break
 
-       ### Version 3   
-       ### #creating a csv file
-       ### csv_file = open('vulnerabilities.csv','w')
-       ### writer = csv.writer(csv_file)
-       ### #wrintng head lable
-       ### writer.writerow(['exploit_id','cve_id'])
-
-        
         #Init variables
-        count =0
-        data = {}
-        
-        #Init json file
-        json_file = open('vulnerabilites.json','w')
+        count = 0
 
+        ###Version 3
+        ###csv_file = open('vulnerabilities.csv','w')
+
+        ####Version 4
+        ####data = {}
+        ####json_file = open('vulnerabilities.json','w')
+        
         #Getting the cells
         for row in table.xpath('//tr'):
-            if count > 100: #only some records to save
+            if count > 10: #only some records to save
                 break
             try:
                 exploit_id = row.xpath('td//text()')[0].extract()
                 cve_id = row.xpath('td//text()')[2].extract()
                 if re.match(r'^EXPLOIT', exploit_id, re.IGNORECASE):
-                    # writting row
-                    ### Version 3
-                    ### writer.writerow([exploit_id,cve_id])
-                    data[exploit_id] = cve_id
+                    ####Version 4
+                    ####data[exploit_id] = cve_id
+
+                    #####Version 5
+                    cursor.execute('INSERT INTO vulns (exploit, cve) VALUES(?,?)', (exploit_id,cve_id))
+                    connection.commit()
+
                     count +=1
             except IndexError:
                 pass
-        ### Version 3    
+        ###Version 3    
         ### csv_file.close()
-        #Serializing json file          
-        json.dump(data,json_file)
-        json_file.close()
+        
+        ####Version 4    
+        ####Serializing json file          
+        ####json.dump(data,json_file)
+        ####json_file.close()
