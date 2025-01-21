d875662650c7fc0a6cd115b88787f6ac121f49aa
ItaSsa <itairassa@gmail.com>
2025-01-21 20:14:53 +0100

    Saving scraping data as a csv file

--- ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
+++ ScrapyUses/vulnerabilites/vulnerabilites/spiders/cve.py
@@ -13,10 +13,16 @@
         1. In the terminal make the download:  wget https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html
         2. Use OS to get file
         Try to time and compare scraping times from website and localy: time scrapy crawl cve
+
+    Version 3 - Saving scraping data as a csv file - Notes:
+        1. import csv module
+        2. Creating a csv file 
+        3. Using writerow to write rows
 '''
 import scrapy
 import os
-
+import csv
+import re
 # Directory of the actual file
 current_dir = os.path.dirname(__file__)
 url = os.path.join(current_dir,'source-EXPLOIT-DB.html')
@@ -33,10 +39,26 @@ class CveSpider(scrapy.Spider):
             if len(child.xpath('tr'))>100:
                 table = child
                 break
+        
+        #creating a csv file
+        csv_file = open('vulnerabilities.csv','w')
+        writer = csv.writer(csv_file)
+        #wrintng head lable
+        writer.writerow(['exploit_id','cve_id'])
 
+        
         #Getting the cells
+        count =0
         for row in table.xpath('//tr'):
+            if count > 100: #only some records to save
+                break
+            print(url)#test
             try:
-                print(row.xpath('td//text()')[0].extract())
+                exploit_id = row.xpath('td//text()')[0].extract()
+                cve_id = row.xpath('td//text()')[2].extract()
+                if re.match(r'^EXPLOIT', exploit_id, re.IGNORECASE):
+                    # writting row
+                    writer.writerow([exploit_id,cve_id])
+                    count +=1
             except IndexError:
                 pass
